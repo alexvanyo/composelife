8dbd24a7bc553b9eae80a70b55645ff902eda2cc
Alex Vanyo <vanyo@google.com>
2021-10-26 19:47:39 -0700

    Move most code out of MainActivity

---
+++ app/src/main/java/com/alexvanyo/composelife/ui/ComposeLifeApp.kt
@@ -0,0 +1,97 @@
+package com.alexvanyo.composelife.ui
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import com.alexvanyo.composelife.MutableGameOfLifeState
+import com.alexvanyo.composelife.NaiveGameOfLifeAlgorithm
+import com.alexvanyo.composelife.ui.theme.ComposeLifeTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+@Composable
+fun ComposeLifeApp() {
+    ComposeLifeTheme {
+        ProvideWindowInsets {
+            val useDarkIcons = MaterialTheme.colors.isLight
+            val systemUiController = rememberSystemUiController()
+
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons
+                )
+            }
+
+            val gameOfLifeState = remember {
+                MutableGameOfLifeState(
+                    cellState = gosperGliderGun
+                )
+            }
+
+            LaunchedEffect(gameOfLifeState) {
+                while (true) {
+                    delay(1000)
+                    withContext(Dispatchers.Default) {
+                        gameOfLifeState.cellState =
+                            NaiveGameOfLifeAlgorithm.computeNextGeneration(gameOfLifeState.cellState)
+                    }
+                }
+            }
+
+            // A surface container using the 'background' color from the theme
+            Surface(color = MaterialTheme.colors.background) {
+                CellUniverse(
+                    gameOfLifeState = gameOfLifeState
+                )
+            }
+        }
+    }
+}
+
+private val gosperGliderGun = setOf(
+    25 to 1,
+    23 to 2,
+    25 to 2,
+    13 to 3,
+    14 to 3,
+    21 to 3,
+    22 to 3,
+    35 to 3,
+    36 to 3,
+    12 to 4,
+    16 to 4,
+    21 to 4,
+    22 to 4,
+    35 to 4,
+    36 to 4,
+    1 to 5,
+    2 to 5,
+    11 to 5,
+    17 to 5,
+    21 to 5,
+    22 to 5,
+    1 to 6,
+    2 to 6,
+    11 to 6,
+    15 to 6,
+    17 to 6,
+    18 to 6,
+    23 to 6,
+    25 to 6,
+    11 to 7,
+    17 to 7,
+    25 to 7,
+    12 to 8,
+    16 to 8,
+    13 to 9,
+    14 to 9,
+)
+
--- app/src/main/java/com/alexvanyo/composelife/ui/MainActivity.kt
+++ app/src/main/java/com/alexvanyo/composelife/ui/MainActivity.kt
@@ -3,21 +3,7 @@ package com.alexvanyo.composelife.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
-import com.alexvanyo.composelife.MutableGameOfLifeState
-import com.alexvanyo.composelife.NaiveGameOfLifeAlgorithm
-import com.alexvanyo.composelife.ui.theme.ComposeLifeTheme
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,81 +12,7 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            ComposeLifeTheme {
-                ProvideWindowInsets {
-                    val useDarkIcons = MaterialTheme.colors.isLight
-                    val systemUiController = rememberSystemUiController()
-
-                    SideEffect {
-                        systemUiController.setSystemBarsColor(
-                            color = Color.Transparent,
-                            darkIcons = useDarkIcons
-                        )
-                    }
-
-                    val gameOfLifeState = remember {
-                        MutableGameOfLifeState(
-                            cellState = gosperGliderGun
-                        )
-                    }
-
-                    LaunchedEffect(gameOfLifeState) {
-                        while (true) {
-                            delay(1000)
-                            withContext(Dispatchers.Default) {
-                                gameOfLifeState.cellState =
-                                    NaiveGameOfLifeAlgorithm.computeNextGeneration(gameOfLifeState.cellState)
-                            }
-                        }
-                    }
-
-                    // A surface container using the 'background' color from the theme
-                    Surface(color = MaterialTheme.colors.background) {
-                        CellUniverse(
-                            gameOfLifeState = gameOfLifeState
-                        )
-                    }
-                }
-            }
+            ComposeLifeApp()
         }
     }
 }
-
-val gosperGliderGun = setOf(
-    25 to 1,
-    23 to 2,
-    25 to 2,
-    13 to 3,
-    14 to 3,
-    21 to 3,
-    22 to 3,
-    35 to 3,
-    36 to 3,
-    12 to 4,
-    16 to 4,
-    21 to 4,
-    22 to 4,
-    35 to 4,
-    36 to 4,
-    1 to 5,
-    2 to 5,
-    11 to 5,
-    17 to 5,
-    21 to 5,
-    22 to 5,
-    1 to 6,
-    2 to 6,
-    11 to 6,
-    15 to 6,
-    17 to 6,
-    18 to 6,
-    23 to 6,
-    25 to 6,
-    11 to 7,
-    17 to 7,
-    25 to 7,
-    12 to 8,
-    16 to 8,
-    13 to 9,
-    14 to 9,
-)
