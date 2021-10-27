18974fce63bf9df894edcc09269f9505eb6b35a0
Alex Vanyo <vanyo@google.com>
2021-10-26 21:49:16 -0700

    Add comments to NaiveGameOfLifeAlgorithm

--- app/src/main/java/com/alexvanyo/composelife/data/NaiveGameOfLifeAlgorithm.kt
+++ app/src/main/java/com/alexvanyo/composelife/data/NaiveGameOfLifeAlgorithm.kt
@@ -5,10 +5,14 @@ import com.alexvanyo.composelife.data.model.CellState
 import com.alexvanyo.composelife.util.getNeighbors
 
 object NaiveGameOfLifeAlgorithm : GameOfLifeAlgorithm {
-    @OptIn(ExperimentalStdlibApi::class)
     override fun computeNextGeneration(cellState: CellState): CellState =
-        cellState.flatMap(IntOffset::getNeighbors).toSet()
+        cellState
+            // Get all neighbors of current living cells
+            .flatMap(IntOffset::getNeighbors)
+            .toSet()
+            // Union those with all living cells, to get all cells that could be alive next round
             .union(cellState)
+            // Filter to the living cells, based on the neighbor count from the previous generation
             .filter { cell ->
                 val neighborCount = cellState.intersect(cell.getNeighbors()).count()
                 neighborCount == 3 || (neighborCount == 2 && cell in cellState)
