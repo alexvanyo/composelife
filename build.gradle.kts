2cf84b8d2fa88343e4417124b2550ce8984dda7c
Alex Vanyo <vanyo@google.com>
2021-11-01 20:15:45 -0700

    Update AGP to 7.2.0-alpha03

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0-alpha02")
+        classpath("com.android.tools.build:gradle:7.2.0-alpha03")
         classpath(kotlin("gradle-plugin", "1.5.31"))
     }
 }
