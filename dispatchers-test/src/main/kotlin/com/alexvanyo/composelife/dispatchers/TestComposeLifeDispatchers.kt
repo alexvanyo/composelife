3f2269db27bd371845570563590cd44a5a74c2d4
Alex Vanyo <vanyo@google.com>
2022-01-30 18:29:52 -0800

    Add comments

--- dispatchers-test/src/main/kotlin/com/alexvanyo/composelife/dispatchers/TestComposeLifeDispatchers.kt
+++ dispatchers-test/src/main/kotlin/com/alexvanyo/composelife/dispatchers/TestComposeLifeDispatchers.kt
@@ -7,12 +7,20 @@ import kotlinx.coroutines.MainCoroutineDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import javax.inject.Inject
 
+/**
+ * A test implementation of [ComposeLifeDispatchers], which delegates [Default] and [IO] to the provided
+ * [TestDispatcher].
+ *
+ * [Main] and [Unconfined] delegate to the default implementations, due to their custom behavior.
+ *
+ * To use [TestDispatcher] as [Dispatchers.Main], use [Dispatchers.setMain].
+ */
 @OptIn(ExperimentalCoroutinesApi::class)
 class TestComposeLifeDispatchers @Inject constructor(
     testDispatcher: TestDispatcher,
 ) : ComposeLifeDispatchers {
     override val Default: CoroutineDispatcher = testDispatcher
     override val Main: MainCoroutineDispatcher = Dispatchers.Main
-    override val Unconfined: CoroutineDispatcher = testDispatcher
+    override val Unconfined: CoroutineDispatcher = Dispatchers.Unconfined
     override val IO: CoroutineDispatcher = testDispatcher
 }
