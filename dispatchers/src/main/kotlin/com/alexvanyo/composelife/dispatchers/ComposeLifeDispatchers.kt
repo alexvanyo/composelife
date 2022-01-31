7c9c0a0a8e06f69c1656b7ff901bb8719fd06385
Alex Vanyo <vanyo@google.com>
2022-01-30 18:29:52 -0800

    Add comments

--- dispatchers/src/main/kotlin/com/alexvanyo/composelife/dispatchers/ComposeLifeDispatchers.kt
+++ dispatchers/src/main/kotlin/com/alexvanyo/composelife/dispatchers/ComposeLifeDispatchers.kt
@@ -5,14 +5,35 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainCoroutineDispatcher
 import javax.inject.Inject
 
+/**
+ * A custom interface version of [Dispatchers], so that dispatchers can be injected.
+ */
 @Suppress("VariableNaming")
 interface ComposeLifeDispatchers {
+    /**
+     * @see Dispatchers.Default
+     */
     val Default: CoroutineDispatcher
+
+    /**
+     * @see Dispatchers.Main
+     */
     val Main: MainCoroutineDispatcher
+
+    /**
+     * @see Dispatchers.Unconfined
+     */
     val Unconfined: CoroutineDispatcher
+
+    /**
+     * @see Dispatchers.IO
+     */
     val IO: CoroutineDispatcher
 }
 
+/**
+ * The default implementation of [ComposeLifeDispatchers], which just delegates to the normal [Dispatchers] versions.
+ */
 class DefaultComposeLifeDispatchers @Inject constructor() : ComposeLifeDispatchers {
     override val Default: CoroutineDispatcher = Dispatchers.Default
     override val Main: MainCoroutineDispatcher = Dispatchers.Main
--- hilt-test/src/main/kotlin/com/alexvanyo/composelife/HiltTestRunner.kt
+++ hilt-test/src/main/kotlin/com/alexvanyo/composelife/HiltTestRunner.kt
@@ -5,6 +5,12 @@ import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
+/**
+ * An [AndroidJUnitRunner] that creates a [HiltTestApplication] to host Hilt tests.
+ *
+ * This is used as the instrumentation runner for on-device tests.
+ */
+@Suppress("unused")
 class HiltTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application =
         super.newApplication(cl, HiltTestApplication::class.java.name, context)
