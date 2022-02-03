5db333b388cb484adbb2c02ba38cc280c4b6fa24
Alex Vanyo <vanyo@google.com>
2022-02-02 16:53:15 -0800

    Add keeper and tests on minified builds

--- build-logic/convention/src/main/kotlin/com.alexvanyo.composelife.android.application.testing.gradle.kts
+++ build-logic/convention/src/main/kotlin/com.alexvanyo.composelife.android.application.testing.gradle.kts
@@ -1,9 +1,27 @@
 import com.alexvanyo.composelife.buildlogic.configureTesting
+import com.slack.keeper.optInToKeeper
 
 plugins {
     id("com.android.application")
+    id("com.slack.keeper")
 }
 
 android {
+    defaultConfig {
+        testBuildType = "staging"
+    }
+
+    androidComponents {
+        beforeVariants { builder ->
+            if (builder.buildType == "staging") {
+                builder.optInToKeeper()
+            }
+        }
+    }
+
     configureTesting(this)
+}
+
+keeper {
+    automaticR8RepoManagement.set(false)
 }
