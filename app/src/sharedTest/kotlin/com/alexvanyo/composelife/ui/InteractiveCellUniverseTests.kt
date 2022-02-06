5acc4f8a00caffc3baa6968e1dfbdf0a5684ce74
Alex Vanyo <vanyo@google.com>
2022-02-06 13:14:02 -0800

    Add ComposeLifeApp tests

--- app/src/sharedTest/kotlin/com/alexvanyo/composelife/ui/InteractiveCellUniverseTests.kt
+++ app/src/sharedTest/kotlin/com/alexvanyo/composelife/ui/InteractiveCellUniverseTests.kt
@@ -1,6 +1,5 @@
 package com.alexvanyo.composelife.ui
 
-import android.content.Context
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsOn
@@ -11,7 +10,6 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.alexvanyo.composelife.R
 import com.alexvanyo.composelife.algorithm.HashLifeAlgorithm
 import com.alexvanyo.composelife.dispatchers.ComposeLifeDispatchers
@@ -19,56 +17,29 @@ import com.alexvanyo.composelife.dispatchers.dateTimeClock
 import com.alexvanyo.composelife.model.rememberTemporalGameOfLifeState
 import com.alexvanyo.composelife.model.rememberTemporalGameOfLifeStateMutator
 import com.alexvanyo.composelife.patterns.SixLongLinePattern
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.testing.HiltAndroidRule
+import com.alexvanyo.composelife.test.BaseAndroidTest
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import javax.inject.Inject
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltAndroidTest
-@RunWith(AndroidJUnit4::class)
-class InteractiveCellUniverseTests {
-
-    @get:Rule
-    val hiltAndroidRule = HiltAndroidRule(this)
+class InteractiveCellUniverseTests : BaseAndroidTest() {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Inject
-    @ApplicationContext
-    lateinit var context: Context
-
-    @Inject
-    lateinit var testDispatcher: TestDispatcher
+    @BindValue
+    val fileProvider = preferencesRule.fileProvider
 
     @Inject
     lateinit var dispatchers: ComposeLifeDispatchers
 
-    @Before
-    fun setup() {
-        hiltAndroidRule.inject()
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @After
-    fun teardown() {
-        Dispatchers.resetMain()
-    }
-
     @Test
-    @OptIn(ExperimentalCoroutinesApi::class)
     fun six_long_line_evolves_correctly() = runTest {
         composeTestRule.mainClock.autoAdvance = false
 
