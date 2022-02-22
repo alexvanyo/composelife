6a7ab64bcd2b432a24056837f12a92c3c7254ab8
Alex Vanyo <vanyo@google.com>
2022-02-21 16:40:27 -0800

    Use select for ConfigurableGameOfLifeAlgorithm

--- algorithm/src/main/kotlin/com/alexvanyo/composelife/algorithm/ConfigurableGameOfLifeAlgorithm.kt
+++ algorithm/src/main/kotlin/com/alexvanyo/composelife/algorithm/ConfigurableGameOfLifeAlgorithm.kt
@@ -3,16 +3,18 @@ package com.alexvanyo.composelife.algorithm
 import com.alexvanyo.composelife.model.CellState
 import com.alexvanyo.composelife.preferences.ComposeLifePreferences
 import com.alexvanyo.composelife.preferences.proto.Algorithm
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.produceIn
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.selects.select
 import javax.inject.Inject
 
 class ConfigurableGameOfLifeAlgorithm @Inject constructor(
@@ -34,22 +36,41 @@ class ConfigurableGameOfLifeAlgorithm @Inject constructor(
             step = step
         )
 
+    @OptIn(FlowPreview::class)
     override fun computeGenerationsWithStep(originalCellState: CellState, step: Int): Flow<CellState> =
         channelFlow {
-            val currentAlgorithm = this@ConfigurableGameOfLifeAlgorithm.currentAlgorithm.shareIn(
-                scope = this,
-                started = SharingStarted.Eagerly,
-                replay = 1
-            )
+            // Start listening to algorithm changes
+            val algorithmChannel = currentAlgorithm
+                .buffer(Channel.CONFLATED) // We only care about the current algorithm
+                .produceIn(this)
 
+            // Setup a receive channel for
+            var cellStateChannel: ReceiveChannel<CellState>? = null
             var cellState = originalCellState
+
             while (currentCoroutineContext().isActive) {
-                cellState = currentAlgorithm.first().computeGenerationWithStep(
-                    cellState = cellState,
-                    step = step
-                )
-                send(cellState)
+                // Select between a new cell state being produced and a new algorithm update coming through
+                // select guarantees that we'll either send a new state, or switch algorithms with the most recently
+                // emitted cell state
+                select<Unit> {
+                    // Bias towards updating the algorithm, it's expected that there will always be a new cell state
+                    // available.
+                    algorithmChannel.onReceive {
+                        // Cancel the ongoing cell state production with the old algorithm, if any
+                        cellStateChannel?.cancel()
+                        cellStateChannel = it.computeGenerationsWithStep(
+                            originalCellState = cellState,
+                            step = step
+                        )
+                            .buffer(Channel.RENDEZVOUS) // Buffer rendezvous, we'll only compute what we need by default
+                            .produceIn(this@channelFlow)
+                    }
+                    cellStateChannel?.onReceive { newCellState ->
+                        cellState = newCellState
+                        send(newCellState)
+                    }
+                }
             }
         }
-            .buffer(Channel.RENDEZVOUS)
+            .buffer(Channel.RENDEZVOUS) // Buffer rendezvous, we'll only compute what we need by default
 }
