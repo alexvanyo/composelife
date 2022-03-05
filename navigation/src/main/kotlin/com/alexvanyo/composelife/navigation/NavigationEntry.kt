fe3c3d4f633f93418c0a7f972a54361bd793027a
Alex Vanyo <vanyo@google.com>
2022-03-05 11:52:12 -0800

    Make minor tweaks to navigation

--- navigation/src/main/kotlin/com/alexvanyo/composelife/navigation/NavigationEntry.kt
+++ navigation/src/main/kotlin/com/alexvanyo/composelife/navigation/NavigationEntry.kt
@@ -3,6 +3,9 @@ package com.alexvanyo.composelife.navigation
 import androidx.compose.runtime.Stable
 import java.util.UUID
 
+/**
+ * An entry in navigation. An entry just needs to have an identifying [id], which is unique among all entries.
+ */
 @Stable
 interface NavigationEntry {
     val id: UUID
