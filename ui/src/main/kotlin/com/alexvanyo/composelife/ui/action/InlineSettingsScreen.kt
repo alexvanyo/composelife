4edd84c70a18ac149df0aef4caa6a0338da2b4f5
Alex Vanyo <vanyo@google.com>
2022-04-03 22:34:12 -0700

    Add initial fullscreen settings

--- ui/src/main/kotlin/com/alexvanyo/composelife/ui/action/InlineSettingsScreen.kt
+++ ui/src/main/kotlin/com/alexvanyo/composelife/ui/action/InlineSettingsScreen.kt
@@ -17,22 +17,37 @@
 package com.alexvanyo.composelife.ui.action
 
 import android.content.res.Configuration
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.alexvanyo.composelife.ui.R
 import com.alexvanyo.composelife.ui.theme.ComposeLifeTheme
 
 @Composable
 fun InlineSettingsScreen(
+    onSeeMoreClicked: () -> Unit,
     modifier: Modifier = Modifier,
+    scrollState: ScrollState = rememberScrollState(),
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(Modifier.weight(1f))
+        TextButton(
+            onClick = onSeeMoreClicked,
+        ) {
+            Text(text = stringResource(R.string.see_more))
+        }
     }
 }
 
@@ -48,7 +63,9 @@ fun InlineSettingsScreen(
 fun InlineSettingsScreenPreview() {
     ComposeLifeTheme {
         Surface {
-            InlineSettingsScreen()
+            InlineSettingsScreen(
+                onSeeMoreClicked = {},
+            )
         }
     }
 }
