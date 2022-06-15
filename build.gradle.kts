14d5c9fb2dc94411cd55d2a6e53285dc5dc16069
Alex Vanyo <vanyo@google.com>
2022-06-14 18:35:08 -0700

    Clean-up root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,17 +18,7 @@ plugins {
     id("com.alexvanyo.composelife.mergejacoco")
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.android.gradlePlugin)
-        classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
-    }
-}
-
 task<Delete>("clean") {
     delete(rootProject.buildDir)
 }
+
