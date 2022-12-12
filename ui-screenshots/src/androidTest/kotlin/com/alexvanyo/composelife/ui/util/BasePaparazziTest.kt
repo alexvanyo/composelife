08d804b932cd2ae45e5982295d7898700420b543
Alex Vanyo <vanyo@google.com>
2022-12-11 19:42:53 -0800

    Update Paparazzi to 1.2.0-SNAPSHOT

--- ui-screenshots/src/androidTest/kotlin/com/alexvanyo/composelife/ui/util/BasePaparazziTest.kt
+++ ui-screenshots/src/androidTest/kotlin/com/alexvanyo/composelife/ui/util/BasePaparazziTest.kt
@@ -29,24 +29,14 @@ import androidx.compose.ui.unit.Density
 import androidx.lifecycle.Lifecycle
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
-import app.cash.paparazzi.androidHome
-import app.cash.paparazzi.detectEnvironment
 import com.alexvanyo.composelife.ui.theme.ComposeLifeTheme
 import com.android.resources.NightMode
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
 import org.junit.runner.RunWith
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
-import kotlin.test.BeforeTest
 
 private val globalPaparazzi = Paparazzi(
-    environment = detectEnvironment().copy(
-        // TODO: Remove when support is added for API 33
-        platformDir = "${androidHome()}/platforms/android-33-paparazzi",
-        compileSdkVersion = 32,
-    ),
     maxPercentDifference = 0.0,
 )
 
@@ -78,18 +68,6 @@ abstract class BasePaparazziTest {
 
     @get:Rule
     val paparazzi = globalPaparazzi
-
-    @BeforeTest
-    fun setup() {
-        // TODO: Remove when support is added for API 33 directly. Forces codename to release
-        val buildVersionClass = Paparazzi::class.java.classLoader!!.loadClass("android.os.Build\$VERSION")
-        val codenameField = buildVersionClass.getDeclaredField("CODENAME")
-        codenameField.isAccessible = true
-        val modifiersField = Field::class.java.getDeclaredField("modifiers")
-        modifiersField.isAccessible = true
-        modifiersField.set(codenameField, codenameField.modifiers and Modifier.FINAL.inv())
-        codenameField.set(null, "REL")
-    }
 
     fun snapshot(composable: @Composable () -> Unit) {
         paparazzi.unsafeUpdateConfig(deviceConfig = deviceConfig)
