798bf2aa9d8362e865644a65e55d5f9e7004ce3a
Alex Vanyo <vanyoalex@gmail.com>
2023-01-03 19:03:15 -0800

    Readd tap to reset watchface

--- wear/src/androidMain/kotlin/com/alexvanyo/composelife/wear/GameOfLifeWatchFaceService.kt
+++ wear/src/androidMain/kotlin/com/alexvanyo/composelife/wear/GameOfLifeWatchFaceService.kt
@@ -19,7 +19,10 @@ package com.alexvanyo.composelife.wear
 import android.view.SurfaceHolder
 import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.ui.platform.AndroidUiDispatcher
+import androidx.wear.watchface.ComplicationSlot
 import androidx.wear.watchface.ComplicationSlotsManager
+import androidx.wear.watchface.TapEvent
+import androidx.wear.watchface.TapType
 import androidx.wear.watchface.WatchFace
 import androidx.wear.watchface.WatchFaceService
 import androidx.wear.watchface.WatchFaceType
@@ -33,6 +36,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -56,6 +60,8 @@ class GameOfLifeWatchFaceService : Hilt_GameOfLifeWatchFaceService() {
         isRunning = false,
         targetStepsPerSecond = 20.0,
     )
+
+    private val isBeingTappedState = MutableStateFlow(false)
 
     override fun onCreate() {
         super.onCreate()
@@ -89,9 +95,10 @@ class GameOfLifeWatchFaceService : Hilt_GameOfLifeWatchFaceService() {
         combine(
             watchState.isVisible,
             watchState.isAmbient,
-        ) { isVisible, isAmbient ->
+            isBeingTappedState,
+        ) { isVisible, isAmbient, isBeingTapped ->
             Snapshot.withMutableSnapshot {
-                temporalGameOfLifeState.setIsRunning(isVisible == true && isAmbient == false)
+                temporalGameOfLifeState.setIsRunning(isVisible == true && isAmbient == false && !isBeingTapped)
             }
         }
             .launchIn(scope)
@@ -105,7 +112,21 @@ class GameOfLifeWatchFaceService : Hilt_GameOfLifeWatchFaceService() {
                 watchState = watchState,
                 temporalGameOfLifeState = temporalGameOfLifeState,
             ),
-        )
+        ).apply {
+            setTapListener(object : WatchFace.TapListener {
+                override fun onTapEvent(tapType: Int, tapEvent: TapEvent, complicationSlot: ComplicationSlot?) {
+                    when (tapType) {
+                        TapType.DOWN -> {
+                            temporalGameOfLifeState.cellState = temporalGameOfLifeState.seedCellState
+                            isBeingTappedState.value = true
+                        }
+                        else -> {
+                            isBeingTappedState.value = false
+                        }
+                    }
+                }
+            })
+        }
     }
 
     override fun onDestroy() {
