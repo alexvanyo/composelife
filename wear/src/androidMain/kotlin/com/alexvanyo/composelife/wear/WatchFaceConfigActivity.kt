735ff8e79a47e9b107b121de8bb3198b6b9e67f5
Alex Vanyo <vanyo@google.com>
2023-01-22 18:29:12 -0800

    WIP watchface config

--- wear/src/androidMain/kotlin/com/alexvanyo/composelife/wear/WatchFaceConfigActivity.kt
+++ wear/src/androidMain/kotlin/com/alexvanyo/composelife/wear/WatchFaceConfigActivity.kt
@@ -32,25 +32,45 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
+import androidx.wear.compose.material.SwipeToDismissBox
 import androidx.wear.watchface.DrawMode
 import androidx.wear.watchface.RenderParameters
 import androidx.wear.watchface.editor.EditorSession
 import androidx.wear.watchface.style.WatchFaceLayer
+import com.alexvanyo.composelife.navigation.BackstackEntry
+import com.alexvanyo.composelife.navigation.MutableBackstackNavigationController
+import com.alexvanyo.composelife.navigation.canNavigateBack
+import com.alexvanyo.composelife.navigation.currentEntry
+import com.alexvanyo.composelife.navigation.popBackstack
+import com.alexvanyo.composelife.snapshotstateset.mutableStateSetOf
+import com.alexvanyo.composelife.updatable.Updatable
+import com.alexvanyo.composelife.wear.theme.ComposeLifeTheme
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
+import java.util.UUID
 
 class WatchFaceConfigActivity : AppCompatActivity() {
 
@@ -59,55 +79,82 @@ class WatchFaceConfigActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var preview by mutableStateOf<Bitmap?>(null)
-
         lifecycleScope.launch {
             editorSession = EditorSession.createOnWatchEditorSession(this@WatchFaceConfigActivity)
-
-            combine(
-                editorSession.userStyle,
-                editorSession.complicationsPreviewData,
-            ) { _, complicationsPreviewData ->
-                yield()
-                editorSession.renderWatchFaceToBitmap(
-                    renderParameters = RenderParameters(
-                        DrawMode.INTERACTIVE,
-                        WatchFaceLayer.ALL_WATCH_FACE_LAYERS,
-                        null
-                    ),
-                    instant = editorSession.previewReferenceInstant,
-                    slotIdToComplicationData = complicationsPreviewData,
-                )
-            }
-                .collect {
-                    preview = it
-                }
         }
 
         setContent {
-            val coroutineScope = rememberCoroutineScope()
+            val watchFaceConfigState = remember(editorSession) {
+                WatchFaceConfigState(editorSession)
+            }
+            LaunchedEffect(watchFaceConfigState) {
+                watchFaceConfigState.update()
+            }
+            ComposeLifeTheme {
+                WatchFaceConfigScreen(watchFaceConfigState)
+            }
+        }
+    }
+}
 
-            LazyColumn {
-                item {
-                    Spacer(modifier = Modifier.height(32.dp))
-                }
-                item {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier.fillMaxWidth(),
-                    ) {
-                        val currentPreview = preview
-                        if (currentPreview != null) {
-                            WatchFacePreview(
-                                previewBitmap = currentPreview,
-                                onComplicationClicked = { id ->
-                                    coroutineScope.launch {
-                                        editorSession.openComplicationDataSourceChooser(id)
-                                    }
-                                }
-                            )
+class WatchFaceConfigState(
+    private val editorSession: EditorSession,
+) : Updatable {
+
+    var preview: Bitmap? by mutableStateOf(null)
+        private set
+
+    suspend fun openComplicationDataSourceChooser(id: Int) {
+        editorSession.openComplicationDataSourceChooser(id)
+    }
+
+    override suspend fun update(): Nothing {
+        combine(
+            editorSession.userStyle,
+            editorSession.complicationsPreviewData,
+        ) { _, complicationsPreviewData ->
+            yield()
+            preview = editorSession.renderWatchFaceToBitmap(
+                renderParameters = RenderParameters(
+                    DrawMode.INTERACTIVE,
+                    WatchFaceLayer.ALL_WATCH_FACE_LAYERS,
+                    null
+                ),
+                instant = editorSession.previewReferenceInstant,
+                slotIdToComplicationData = complicationsPreviewData,
+            )
+        }
+            .collect {}
+
+        error("combine should not complete normally")
+    }
+}
+
+@Composable
+fun WatchFaceConfigScreen(
+    state: WatchFaceConfigState,
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    LazyColumn {
+        item {
+            Spacer(modifier = Modifier.height(32.dp))
+        }
+        item {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                val currentPreview = state.preview
+                if (currentPreview != null) {
+                    WatchFacePreview(
+                        previewBitmap = currentPreview,
+                        onComplicationClicked = { id ->
+                            coroutineScope.launch {
+                                state.openComplicationDataSourceChooser(id)
+                            }
                         }
-                    }
+                    )
                 }
             }
         }
@@ -118,20 +165,37 @@ class WatchFaceConfigActivity : AppCompatActivity() {
 fun WatchFacePreview(
     previewBitmap: Bitmap,
     onComplicationClicked: (Int) -> Unit,
-) {
-    val previewImageBitmap = remember(previewBitmap) {
+    modifier: Modifier = Modifier,
+) = WatchFacePreview(
+    previewImageBitmap = remember(previewBitmap) {
         previewBitmap.asImageBitmap()
-    }
+    },
+    onComplicationClicked = onComplicationClicked,
+    modifier = modifier,
+)
+
+@Composable
+fun WatchFacePreview(
+    previewImageBitmap: ImageBitmap,
+    onComplicationClicked: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
     val aspectRatio = previewImageBitmap.width.toFloat() / previewImageBitmap.height
     Box(
-        modifier = Modifier
-            .fillMaxWidth(0.6f)
-            .aspectRatio(aspectRatio)
+        modifier = modifier.aspectRatio(aspectRatio)
     ) {
         Image(
             bitmap = previewImageBitmap,
             contentDescription = "Preview",
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .then(
+                    if (LocalConfiguration.current.isScreenRound) {
+                        Modifier.clip(CircleShape)
+                    } else {
+                        Modifier
+                    }
+                )
         )
 
         GameOfLifeComplication.values.forEach { gameOfLifeComplication ->
@@ -171,5 +235,64 @@ fun WatchFacePreview(
                 )
             }
         }
+    }
+}
+
+@DevicePreviews
+@Composable
+fun WatchFacePreviewPreview() {
+    WatchFacePreview(
+        previewImageBitmap = ImageBitmap.imageResource(R.drawable.watchface_square),
+        onComplicationClicked = {},
+    )
+}
+
+@Composable
+fun <T> WearNavigationHost(
+    navigationController: MutableBackstackNavigationController<T>,
+    modifier: Modifier = Modifier,
+    content: @Composable (BackstackEntry<out T>) -> Unit,
+) {
+    val stateHolder = rememberSaveableStateHolder()
+    val allKeys = rememberSaveable(
+        saver = listSaver(
+            save = { it.map(UUID::toString) },
+            restore = {
+                mutableStateSetOf<UUID>().apply {
+                    addAll(it.map(UUID::fromString))
+                }
+            },
+        ),
+    ) { mutableStateSetOf<UUID>() }
+
+    SwipeToDismissBox(
+        onDismissed = { navigationController.popBackstack() },
+        backgroundKey = navigationController.currentEntry.previous?.id ?: remember { UUID.randomUUID() },
+        contentKey = navigationController.currentEntry.id,
+        hasBackground = navigationController.canNavigateBack,
+        modifier = modifier,
+    ) { isBackground ->
+        val entry = if (isBackground) {
+            checkNotNull(navigationController.currentEntry.previous) {
+                "Current entry had no previous, should not be showing background!"
+            }
+        } else {
+            navigationController.currentEntry
+        }
+
+        key(entry.id) {
+            stateHolder.SaveableStateProvider(key = entry.id) {
+                content(entry)
+            }
+        }
+    }
+
+    val keySet: Set<UUID> = navigationController.entryMap.keys.toSet()
+
+    LaunchedEffect(keySet) {
+        // Remove the state for a given key if it doesn't correspond to an entry in the backstack map
+        (allKeys - keySet).forEach(stateHolder::removeState)
+        // Keep track of the ids we've seen, to know which ones we may need to clear out later.
+        allKeys.addAll(keySet)
     }
 }
