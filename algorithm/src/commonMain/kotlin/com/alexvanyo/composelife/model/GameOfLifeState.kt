adc8d5c98f131380855e4565697c5c13fadfafc6
Alex Vanyo <vanyo@google.com>
2023-04-12 19:24:32 -0700

    Document GameOfLifeState

--- algorithm/src/commonMain/kotlin/com/alexvanyo/composelife/model/GameOfLifeState.kt
+++ algorithm/src/commonMain/kotlin/com/alexvanyo/composelife/model/GameOfLifeState.kt
@@ -16,33 +16,56 @@
 
 package com.alexvanyo.composelife.model
 
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.IntOffset
 
+/**
+ * The state describing a particular game of life pattern given by the [CellState].
+ */
+@Stable
 interface GameOfLifeState {
     val cellState: CellState
 }
 
+/**
+ * A simple implementation of an immutable [GameOfLifeState].
+ */
 private class ImmutableGameOfLifeState(
     override val cellState: CellState,
 ) : GameOfLifeState
 
+/**
+ * Creates a fixed [GameOfLifeState] for the given [cellState].
+ */
 fun GameOfLifeState(cellState: CellState): GameOfLifeState = ImmutableGameOfLifeState(cellState)
 
+/**
+ * A mutable [GameOfLifeState] where [cellState] can be modified.
+ */
 interface MutableGameOfLifeState : GameOfLifeState {
     override var cellState: CellState
 }
 
+/**
+ * Creates a [MutableGameOfLifeState] with the given initial [cellState]/
+ */
 fun MutableGameOfLifeState(cellState: CellState): MutableGameOfLifeState = MutableGameOfLifeStateImpl(cellState)
 
+/**
+ * A simple implementation of a mutable [GameOfLifeState].
+ */
 private class MutableGameOfLifeStateImpl(
     cellState: CellState,
 ) : MutableGameOfLifeState {
     override var cellState by mutableStateOf(cellState)
 }
 
+/**
+ * Modifies the given [MutableGameOfLifeState] by setting the cell state at the given [cellCoordinate] to [isAlive].
+ */
 fun MutableGameOfLifeState.setCellState(cellCoordinate: IntOffset, isAlive: Boolean) {
     cellState = cellState.withCell(cellCoordinate, isAlive)
 }
