35e09e534501aebf12229b4e1cf18f8ef63266c0
Alex Vanyo <vanyo@google.com>
2023-07-26 20:02:53 -0700

    Update for new dividers

--- ui-app/src/androidMain/kotlin/com/alexvanyo/composelife/ui/app/action/settings/QuickAccessSettingHeader.kt
+++ ui-app/src/androidMain/kotlin/com/alexvanyo/composelife/ui/app/action/settings/QuickAccessSettingHeader.kt
@@ -23,8 +23,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.OpenInFull
 import androidx.compose.material.icons.outlined.BookmarkBorder
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconToggleButton
@@ -55,7 +55,7 @@ fun QuickAccessSettingHeader(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Divider(
+        HorizontalDivider(
             modifier = Modifier.weight(1f),
             color = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
         )
@@ -76,7 +76,7 @@ fun QuickAccessSettingHeader(
                     )
                 }
             }
-            Divider(
+            HorizontalDivider(
                 color = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
                 modifier = Modifier.width(16.dp),
             )
@@ -113,7 +113,7 @@ fun QuickAccessSettingHeader(
             }
         }
 
-        Divider(
+        HorizontalDivider(
             color = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
             modifier = Modifier.width(16.dp),
         )
