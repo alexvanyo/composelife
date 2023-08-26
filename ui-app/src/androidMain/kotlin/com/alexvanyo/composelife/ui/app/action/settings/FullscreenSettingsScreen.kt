7821486b84c6ab8038ebbcc7312dea229a8efb12
Alex Vanyo <vanyo@google.com>
2023-08-26 16:56:32 -0700

    Add draggable pane

--- ui-app/src/androidMain/kotlin/com/alexvanyo/composelife/ui/app/action/settings/FullscreenSettingsScreen.kt
+++ ui-app/src/androidMain/kotlin/com/alexvanyo/composelife/ui/app/action/settings/FullscreenSettingsScreen.kt
@@ -17,8 +17,21 @@
 
 package com.alexvanyo.composelife.ui.app.action.settings
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.anchoredDraggable
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -29,6 +42,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -36,9 +50,12 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.windowInsetsStartWidth
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -72,13 +89,20 @@ import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
@@ -90,10 +114,12 @@ import com.alexvanyo.composelife.ui.app.entrypoints.WithPreviewDependencies
 import com.alexvanyo.composelife.ui.app.theme.ComposeLifeTheme
 import com.alexvanyo.composelife.ui.util.AnimatedContent
 import com.alexvanyo.composelife.ui.util.Crossfade
+import com.alexvanyo.composelife.ui.util.Layout
 import com.alexvanyo.composelife.ui.util.MobileDevicePreviews
 import com.alexvanyo.composelife.ui.util.PredictiveBackState
 import com.alexvanyo.composelife.ui.util.TargetState
 import com.alexvanyo.composelife.ui.util.predictiveBackHandler
+import com.livefront.sealedenum.GenSealedEnum
 import kotlin.math.roundToInt
 
 interface FullscreenSettingsScreenInjectEntryPoint :
@@ -103,6 +129,7 @@ interface FullscreenSettingsScreenLocalEntryPoint :
     SettingUiLocalEntryPoint
 
 context(FullscreenSettingsScreenInjectEntryPoint, FullscreenSettingsScreenLocalEntryPoint)
+@OptIn(ExperimentalFoundationApi::class)
 @Suppress("LongMethod", "CyclomaticComplexMethod")
 @Composable
 fun FullscreenSettingsScreen(
@@ -170,43 +197,189 @@ fun FullscreenSettingsScreen(
         }
     }
 
-    if (showListAndDetail()) {
-        Row(modifier = modifier) {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.End)),
-            ) {
-                listContent()
-            }
+    val density = LocalDensity.current
+    val anchoredDraggableState = rememberSaveable(
+        saver = AnchoredDraggableState.Saver(
+            positionalThreshold = { totalDistance -> totalDistance * 0.5f },
+            velocityThreshold = { with(density) { 200.dp.toPx() } },
+            animationSpec = spring(),
+        ),
+    ) {
+        AnchoredDraggableState(
+            initialValue = 0.5f,
+            positionalThreshold = { totalDistance -> totalDistance * 0.5f },
+            velocityThreshold = { with(density) { 200.dp.toPx() } },
+            animationSpec = spring(),
+        )
+    }
 
-            Column(
-                Modifier
-                    .weight(1f)
-                    .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.Start))
-                    .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal))
-                    .padding(
-                        top = 4.dp,
-                        start = 8.dp,
-                        end = 8.dp,
-                        bottom = 16.dp,
-                    ),
-            ) {
-                Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
-                Surface(
-                    color = MaterialTheme.colorScheme.secondaryContainer,
-                    shape = RoundedCornerShape(16.dp),
+    val minPaneWidth = 200.dp
+
+    if (showListAndDetail()) {
+        Layout(
+            layoutIdTypes = ListAndDetailLayoutTypes.sealedEnum,
+            modifier = modifier,
+            content = {
+                Spacer(
                     modifier = Modifier
-                        .weight(1f)
-                        .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)),
+                        .layoutId(ListAndDetailLayoutTypes.StartInsets)
+                        .windowInsetsStartWidth(WindowInsets.safeDrawing),
+                )
+                Spacer(
+                    modifier = Modifier
+                        .layoutId(ListAndDetailLayoutTypes.EndInsets)
+                        .windowInsetsEndWidth(WindowInsets.safeDrawing),
+                )
+
+                Box(
+                    modifier = Modifier
+                        .layoutId(ListAndDetailLayoutTypes.List)
+                        .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.End)),
                 ) {
-                    Crossfade(targetState = TargetState.Single(fullscreen.settingsCategory)) { settingsCategory ->
-                        detailContent(settingsCategory)
+                    listContent()
+                }
+
+                Column(
+                    Modifier
+                        .layoutId(ListAndDetailLayoutTypes.Detail)
+                        .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.Start))
+                        .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal))
+                        .padding(
+                            top = 4.dp,
+                            start = 8.dp,
+                            end = 8.dp,
+                            bottom = 16.dp,
+                        ),
+                ) {
+                    Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
+                    Surface(
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        shape = RoundedCornerShape(16.dp),
+                        modifier = Modifier
+                            .weight(1f)
+                            .consumeWindowInsets(WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)),
+                    ) {
+                        Crossfade(targetState = TargetState.Single(fullscreen.settingsCategory)) { settingsCategory ->
+                            detailContent(settingsCategory)
+                        }
+                    }
+                    Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.safeDrawing))
+                }
+
+                Box(
+                    modifier = Modifier
+                        .layoutId(ListAndDetailLayoutTypes.Divider)
+                        .fillMaxHeight()
+                        .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    val handleInteractionSource = remember { MutableInteractionSource() }
+
+                    Box(
+                        modifier = Modifier
+                            .size(64.dp)
+                            .hoverable(
+                                interactionSource = handleInteractionSource,
+                            )
+                            .anchoredDraggable(
+                                state = anchoredDraggableState,
+                                orientation = Orientation.Horizontal,
+                                interactionSource = handleInteractionSource,
+                            )
+                            .pointerHoverIcon(PointerIcon.Hand),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        val isHandleDragged by handleInteractionSource.collectIsDraggedAsState()
+                        val isHandleHovered by handleInteractionSource.collectIsHoveredAsState()
+                        val isHandlePressed by handleInteractionSource.collectIsPressedAsState()
+                        val isHandleActive = isHandleDragged || isHandleHovered || isHandlePressed
+                        val handleWidth by animateDpAsState(
+                            targetValue = if (isHandleActive) 12.dp else 4.dp,
+                            label = "handleWidth",
+                        )
+                        val handleColor by animateColorAsState(
+                            targetValue = if (isHandleActive) {
+                                MaterialTheme.colorScheme.onSurface
+                            } else {
+                                MaterialTheme.colorScheme.outline
+                            },
+                            label = "handleColor",
+                        )
+                        Canvas(
+                            modifier = Modifier.fillMaxSize(),
+                        ) {
+                            val handleSize = DpSize(handleWidth, 48.dp).toSize()
+                            val handleOffset = Offset(
+                                (size.width - handleSize.width) / 2f,
+                                (size.height - handleSize.height) / 2f,
+                            )
+                            drawRoundRect(
+                                color = handleColor,
+                                topLeft = handleOffset,
+                                size = handleSize,
+                                cornerRadius = CornerRadius(handleSize.width / 2),
+                            )
+                        }
                     }
                 }
-                Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.safeDrawing))
-            }
-        }
+            },
+            measurePolicy = { measurables, constraints ->
+                val startInsetsPlaceable = measurables
+                    .getValue(ListAndDetailLayoutTypes.StartInsets)
+                    .measure(constraints.copy(minWidth = 0))
+
+                val endInsetsPlaceable = measurables
+                    .getValue(ListAndDetailLayoutTypes.EndInsets)
+                    .measure(constraints.copy(minWidth = 0))
+
+                val minPaneWidthPx = minPaneWidth.toPx()
+
+                val freeSpace = constraints.maxWidth -
+                    startInsetsPlaceable.width -
+                    endInsetsPlaceable.width -
+                    minPaneWidthPx * 2
+
+                layout(constraints.maxWidth, constraints.maxHeight) {
+                    val minAnchoredDraggablePosition = 0f
+                    val maxAnchoredDraggablePosition = freeSpace.coerceAtLeast(0f)
+
+                    anchoredDraggableState.updateAnchors(
+                        newAnchors = ContinuousDraggableAnchors(
+                            minAnchoredDraggablePosition = minAnchoredDraggablePosition,
+                            maxAnchoredDraggablePosition = maxAnchoredDraggablePosition,
+                        ),
+                        newTarget = anchoredDraggableState.targetValue,
+                    )
+
+                    val currentFraction = checkNotNull(
+                        anchoredDraggableState.anchors.closestAnchor(
+                            anchoredDraggableState.requireOffset(),
+                        ),
+                    )
+
+                    val listPaneExtraSpace = freeSpace * currentFraction
+                    val listPaneWidth = (startInsetsPlaceable.width + minPaneWidthPx + listPaneExtraSpace).roundToInt()
+                    val detailPaneWidth = constraints.maxWidth - listPaneWidth
+
+                    val listPanePlaceable = measurables
+                        .getValue(ListAndDetailLayoutTypes.List)
+                        .measure(constraints.copy(minWidth = listPaneWidth, maxWidth = listPaneWidth))
+
+                    val detailPanePlaceable = measurables
+                        .getValue(ListAndDetailLayoutTypes.Detail)
+                        .measure(constraints.copy(minWidth = detailPaneWidth, maxWidth = detailPaneWidth))
+
+                    listPanePlaceable.placeRelative(0, 0)
+                    detailPanePlaceable.placeRelative(listPaneWidth, 0)
+
+                    val dividerPlaceable = measurables
+                        .getValue(ListAndDetailLayoutTypes.Divider)
+                        .measure(constraints)
+
+                    dividerPlaceable.placeRelative(listPaneWidth - dividerPlaceable.width / 2, 0)
+                }
+            },
+        )
     } else {
         AnimatedContent(
             targetState = when (predictiveBackState) {
@@ -227,6 +400,66 @@ fun FullscreenSettingsScreen(
             }
         }
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+data class ContinuousDraggableAnchors(
+    private val minAnchoredDraggablePosition: Float,
+    private val maxAnchoredDraggablePosition: Float,
+) : DraggableAnchors<Float> {
+    override val size: Int = 1
+
+    override fun closestAnchor(position: Float): Float =
+        (
+            position.coerceIn(minAnchoredDraggablePosition, maxAnchoredDraggablePosition) -
+                minAnchoredDraggablePosition
+            ) /
+            (maxAnchoredDraggablePosition - minAnchoredDraggablePosition)
+
+    override fun closestAnchor(position: Float, searchUpwards: Boolean): Float? =
+        if (searchUpwards) {
+            if (position <= maxAnchoredDraggablePosition) {
+                (
+                    position.coerceIn(minAnchoredDraggablePosition, maxAnchoredDraggablePosition) -
+                        minAnchoredDraggablePosition
+                    ) /
+                    (maxAnchoredDraggablePosition - minAnchoredDraggablePosition)
+            } else {
+                null
+            }
+        } else {
+            if (position >= minAnchoredDraggablePosition) {
+                (
+                    position.coerceIn(minAnchoredDraggablePosition, maxAnchoredDraggablePosition) -
+                        minAnchoredDraggablePosition
+                    ) /
+                    (maxAnchoredDraggablePosition - minAnchoredDraggablePosition)
+            } else {
+                null
+            }
+        }
+
+    override fun maxAnchor(): Float = maxAnchoredDraggablePosition
+
+    override fun minAnchor(): Float = minAnchoredDraggablePosition
+
+    override fun positionOf(value: Float): Float =
+        value * (maxAnchoredDraggablePosition - minAnchoredDraggablePosition) +
+            minAnchoredDraggablePosition
+
+    override fun hasAnchorFor(value: Float): Boolean =
+        value in minAnchoredDraggablePosition..maxAnchoredDraggablePosition
+}
+
+sealed interface ListAndDetailLayoutTypes {
+    data object StartInsets : ListAndDetailLayoutTypes
+    data object EndInsets : ListAndDetailLayoutTypes
+    data object List : ListAndDetailLayoutTypes
+    data object Detail : ListAndDetailLayoutTypes
+    data object Divider : ListAndDetailLayoutTypes
+
+    @GenSealedEnum
+    companion object
 }
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
