2c8972fb18bf300960c5bde9cb7e1d40b3fe4074
Alex Vanyo <vanyo@google.com>
2024-01-23 20:10:28 -0800

    Re-align Draggable2D

--- ui-common/src/commonMain/kotlin/com/alexvanyo/composelife/ui/util/AnchoredDraggable2D.kt
+++ ui-common/src/commonMain/kotlin/com/alexvanyo/composelife/ui/util/AnchoredDraggable2D.kt
@@ -21,8 +21,13 @@ import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.calculateTargetValue
 import androidx.compose.animation.core.exponentialDecay
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.MutatorMutex
+import androidx.compose.foundation.gestures.Drag2DScope
+import androidx.compose.foundation.gestures.Draggable2DState
 import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.draggable2D
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Stable
@@ -43,9 +48,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import java.util.concurrent.atomic.AtomicReference
 
 /**
  * Structure that represents the anchors of a [AnchoredDraggable2DState].
@@ -136,6 +138,7 @@ fun <T : Any> DraggableAnchors2D(
  * @param interactionSource Optional [MutableInteractionSource] that will passed on to
  * the internal [Modifier.draggable].
  */
+@OptIn(ExperimentalFoundationApi::class)
 fun <T> Modifier.anchoredDraggable2D(
     state: AnchoredDraggable2DState<T>,
     enabled: Boolean = true,
@@ -214,6 +217,7 @@ class AnchoredDraggable2DState<T>(
 
     private val dragMutex = MutatorMutex()
 
+    @OptIn(ExperimentalFoundationApi::class)
     internal val draggableState = object : Draggable2DState {
 
         private val dragScope = object : Drag2DScope {
@@ -663,165 +667,4 @@ private class MapDraggableAnchors2D<T>(private val anchors: Map<T, Offset>) : Dr
     override fun hashCode() = 31 * anchors.hashCode()
 
     override fun toString() = "MapDraggableAnchors2D($anchors)"
-}
-
-/**
- * Used in place of the standard Job cancellation pathway to avoid reflective
- * javaClass.simpleName lookups to build the exception message and stack trace collection.
- * Remove if these are changed in kotlinx.coroutines.
- */
-private class MutationInterruptedException : CancellationException("Mutation interrupted") {
-    override fun fillInStackTrace(): Throwable {
-        // Avoid null.clone() on Android <= 6.0 when accessing stackTrace
-        stackTrace = emptyArray()
-        return this
-    }
-}
-
-/**
- * Mutual exclusion for UI state mutation over time.
- *
- * [mutate] permits interruptible state mutation over time using a standard [MutatePriority].
- * A [MutatorMutex] enforces that only a single writer can be active at a time for a particular
- * state resource. Instead of queueing callers that would acquire the lock like a traditional
- * [Mutex], new attempts to [mutate] the guarded state will either cancel the current mutator or
- * if the current mutator has a higher priority, the new caller will throw [CancellationException].
- *
- * [MutatorMutex] should be used for implementing hoisted state objects that many mutators may
- * want to manipulate over time such that those mutators can coordinate with one another. The
- * [MutatorMutex] instance should be hidden as an implementation detail. For example:
- *
- * @sample androidx.compose.foundation.samples.mutatorMutexStateObject
- */
-@Stable
-private class MutatorMutex {
-    private class Mutator(val priority: MutatePriority, val job: Job) {
-        fun canInterrupt(other: Mutator) = priority >= other.priority
-
-        fun cancel() = job.cancel(MutationInterruptedException())
-    }
-
-    private val currentMutator = AtomicReference<Mutator?>(null)
-    private val mutex = Mutex()
-
-    private fun tryMutateOrCancel(mutator: Mutator) {
-        while (true) {
-            val oldMutator = currentMutator.get()
-            if (oldMutator == null || mutator.canInterrupt(oldMutator)) {
-                if (currentMutator.compareAndSet(oldMutator, mutator)) {
-                    oldMutator?.cancel()
-                    break
-                }
-            } else {
-                throw CancellationException("Current mutation had a higher priority")
-            }
-        }
-    }
-
-    /**
-     * Enforce that only a single caller may be active at a time.
-     *
-     * If [mutate] is called while another call to [mutate] or [mutateWith] is in progress, their
-     * [priority] values are compared. If the new caller has a [priority] equal to or higher than
-     * the call in progress, the call in progress will be cancelled, throwing
-     * [CancellationException] and the new caller's [block] will be invoked. If the call in
-     * progress had a higher [priority] than the new caller, the new caller will throw
-     * [CancellationException] without invoking [block].
-     *
-     * @param priority the priority of this mutation; [MutatePriority.Default] by default. Higher
-     * priority mutations will interrupt lower priority mutations.
-     * @param block mutation code to run mutually exclusive with any other call to [mutate] or
-     * [mutateWith].
-     */
-    suspend fun <R> mutate(
-        priority: MutatePriority = MutatePriority.Default,
-        block: suspend () -> R,
-    ) = coroutineScope {
-        val mutator = Mutator(priority, coroutineContext[Job]!!)
-
-        tryMutateOrCancel(mutator)
-
-        mutex.withLock {
-            try {
-                block()
-            } finally {
-                currentMutator.compareAndSet(mutator, null)
-            }
-        }
-    }
-
-    /**
-     * Enforce that only a single caller may be active at a time.
-     *
-     * If [mutateWith] is called while another call to [mutate] or [mutateWith] is in progress,
-     * their [priority] values are compared. If the new caller has a [priority] equal to or
-     * higher than the call in progress, the call in progress will be cancelled, throwing
-     * [CancellationException] and the new caller's [block] will be invoked. If the call in
-     * progress had a higher [priority] than the new caller, the new caller will throw
-     * [CancellationException] without invoking [block].
-     *
-     * This variant of [mutate] calls its [block] with a [receiver], removing the need to create
-     * an additional capturing lambda to invoke it with a receiver object. This can be used to
-     * expose a mutable scope to the provided [block] while leaving the rest of the state object
-     * read-only. For example:
-     *
-     * @sample androidx.compose.foundation.samples.mutatorMutexStateObjectWithReceiver
-     *
-     * @param receiver the receiver `this` that [block] will be called with
-     * @param priority the priority of this mutation; [MutatePriority.Default] by default. Higher
-     * priority mutations will interrupt lower priority mutations.
-     * @param block mutation code to run mutually exclusive with any other call to [mutate] or
-     * [mutateWith].
-     */
-    suspend fun <T, R> mutateWith(
-        receiver: T,
-        priority: MutatePriority = MutatePriority.Default,
-        block: suspend T.() -> R,
-    ) = coroutineScope {
-        val mutator = Mutator(priority, coroutineContext[Job]!!)
-
-        tryMutateOrCancel(mutator)
-
-        mutex.withLock {
-            try {
-                receiver.block()
-            } finally {
-                currentMutator.compareAndSet(mutator, null)
-            }
-        }
-    }
-
-    /**
-     * Attempt to mutate synchronously if there is no other active caller.
-     * If there is no other active caller, the [block] will be executed in a lock. If there is
-     * another active caller, this method will return false, indicating that the active caller
-     * needs to be cancelled through a [mutate] or [mutateWith] call with an equal or higher
-     * mutation priority.
-     *
-     * Calls to [mutate] and [mutateWith] will suspend until execution of the [block] has finished.
-     *
-     * @param block mutation code to run mutually exclusive with any other call to [mutate],
-     * [mutateWith] or [tryMutate].
-     * @return true if the [block] was executed, false if there was another active caller and the
-     * [block] was not executed.
-     */
-    inline fun tryMutate(block: () -> Unit): Boolean {
-        val didLock = tryLock()
-        if (didLock) {
-            try {
-                block()
-            } finally {
-                unlock()
-            }
-        }
-        return didLock
-    }
-
-    @PublishedApi
-    internal fun tryLock(): Boolean = mutex.tryLock()
-
-    @PublishedApi
-    internal fun unlock() {
-        mutex.unlock()
-    }
 }
