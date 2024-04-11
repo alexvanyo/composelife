8fa50225ad51e5da5288de8febff3e2828cebcd8
Alex Vanyo <vanyo@google.com>
2024-04-10 18:55:24 -0700

    Update Compose to snapshot

---
+++ ui-common/src/commonMain/kotlin/com/alexvanyo/composelife/ui/util/DeferredTargetAnimation.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alexvanyo.composelife.ui.util
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.TwoWayConverter
+import androidx.compose.animation.core.spring
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+/**
+ * TODO: Replace with upstream DeferredTargetAnimation
+ */
+class DeferredTargetAnimation<T, V : AnimationVector>(
+    private val vectorConverter: TwoWayConverter<T, V>,
+) {
+    /**
+     * Returns the target value from the most recent [updateTarget] call.
+     */
+    val pendingTarget: T?
+        get() = _pendingTarget
+
+    private var _pendingTarget: T? by mutableStateOf(null)
+    private val target: T?
+        get() = animatable?.targetValue
+    private var animatable: Animatable<T, V>? = null
+
+    /**
+     * [updateTarget] sets up an animation, or updates an already running animation, based on the
+     * [target] in the given [coroutineScope]. [pendingTarget] will be updated to track the last
+     * seen [target].
+     *
+     * [updateTarget] will return the current value of the animation after launching the animation
+     * in the given [coroutineScope].
+     *
+     * @return current value of the animation
+     */
+    fun updateTarget(
+        target: T,
+        coroutineScope: CoroutineScope,
+        animationSpec: FiniteAnimationSpec<T> = spring(),
+    ): T {
+        _pendingTarget = target
+        val anim = animatable ?: Animatable(target, vectorConverter).also { animatable = it }
+        coroutineScope.launch {
+            if (anim.targetValue != _pendingTarget) {
+                anim.animateTo(target, animationSpec)
+            }
+        }
+        return anim.value
+    }
+
+    /**
+     * [isIdle] returns true when the animation has finished running and reached its
+     * [pendingTarget], or when the animation has not been set up (i.e. [updateTarget] has never
+     * been called).
+     */
+    val isIdle: Boolean
+        get() = _pendingTarget == target && animatable?.isRunning != true
+}
