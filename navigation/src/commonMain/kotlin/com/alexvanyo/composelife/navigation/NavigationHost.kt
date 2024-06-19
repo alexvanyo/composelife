9115fb65960a1a0b3edb6e423f25269426b8ba0a
Alex Vanyo <vanyo@google.com>
2024-06-19 12:13:23 -0700

    Fix NavigationHost for desktop

--- navigation/src/commonMain/kotlin/com/alexvanyo/composelife/navigation/NavigationHost.kt
+++ navigation/src/commonMain/kotlin/com/alexvanyo/composelife/navigation/NavigationHost.kt
@@ -16,23 +16,12 @@
 
 package com.alexvanyo.composelife.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.ContentTransform
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 /**
  * The primary composable for displaying a [NavigationState].
- *
- * This automatically animated transitions between content, which is rendered for the current top-most entry with the
- * id of [NavigationState.currentEntryId] as specified in [content].
  *
  * The state for each entry is independently saved via [rememberSaveableStateHolder], and the corresponding state is
  * cleared when the keys are observed to no longer be in the backstack map.
@@ -41,18 +30,8 @@ import androidx.compose.ui.Modifier
 fun <T : NavigationEntry> NavigationHost(
     navigationState: NavigationState<T>,
     modifier: Modifier = Modifier,
-    transitionSpec: AnimatedContentTransitionScope<T>.() -> ContentTransform = {
-        (
-            fadeIn(animationSpec = tween(220, delayMillis = 90)) +
-                scaleIn(initialScale = 0.92f, animationSpec = tween(220, delayMillis = 90))
-            )
-            .togetherWith(fadeOut(animationSpec = tween(90)))
-    },
-    contentAlignment: Alignment = Alignment.TopStart,
     content: @Composable (T) -> Unit,
-) = AnimatedContentNavigationFrame(
+) = JumpcutNavigationFrame(
     renderableNavigationState = associateWithRenderablePanes(navigationState, content),
     modifier = modifier,
-    transitionSpec = transitionSpec,
-    contentAlignment = contentAlignment,
 )
