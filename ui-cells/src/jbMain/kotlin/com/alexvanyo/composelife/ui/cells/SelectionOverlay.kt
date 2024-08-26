778b73a43b8eb9138ec474864588ab581c12c554
Alex Vanyo <vanyo@google.com>
2024-08-25 17:37:44 -0700

    Add API 27- compatibility to dashed line drawing

--- ui-cells/src/jbMain/kotlin/com/alexvanyo/composelife/ui/cells/SelectionOverlay.kt
+++ ui-cells/src/jbMain/kotlin/com/alexvanyo/composelife/ui/cells/SelectionOverlay.kt
@@ -16,6 +16,7 @@
 
 package com.alexvanyo.composelife.ui.cells
 
+import androidx.annotation.FloatRange
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector2D
 import androidx.compose.animation.core.VectorConverter
@@ -55,9 +56,14 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultBlendMode
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
@@ -632,13 +638,11 @@ fun SelectingBox(
         drawSelectionRect(
             selectionColor = selectionColor,
             strokeWidth = 2.dp.toPx(),
-            pathEffect = PathEffect.dashPathEffect(
-                floatArrayOf(
-                    24.dp.toPx(),
-                    24.dp.toPx(),
-                ),
-                phase = 12.dp.toPx(),
+            intervals = floatArrayOf(
+                24.dp.toPx(),
+                24.dp.toPx(),
             ),
+            phase = 12.dp.toPx(),
         )
     }
 }
@@ -652,68 +656,46 @@ fun SelectingBox(
 private fun DrawScope.drawSelectionRect(
     selectionColor: Color,
     strokeWidth: Float,
-    pathEffect: PathEffect,
+    intervals: FloatArray,
+    phase: Float = 0f,
     rect: Rect = size.toRect(),
 ) {
     // Draw the selection outlines in a way that is symmetric, and hides the extra of the lines near the
     // corners with the selection handles
-    drawLine(
-        color = selectionColor,
-        start = rect.topCenter,
-        end = rect.topLeft,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.topCenter,
-        end = rect.topRight,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.centerLeft,
-        end = rect.topLeft,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.centerLeft,
-        end = rect.bottomLeft,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.bottomCenter,
-        end = rect.bottomLeft,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.bottomCenter,
-        end = rect.bottomRight,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.centerRight,
-        end = rect.topRight,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
-    drawLine(
-        color = selectionColor,
-        start = rect.centerRight,
-        end = rect.bottomRight,
-        strokeWidth = strokeWidth,
-        pathEffect = pathEffect,
-    )
+    listOf(
+        rect.topCenter to rect.topLeft,
+        rect.topCenter to rect.topRight,
+        rect.centerLeft to rect.topLeft,
+        rect.centerLeft to rect.bottomLeft,
+        rect.bottomCenter to rect.bottomLeft,
+        rect.bottomCenter to rect.bottomRight,
+        rect.centerRight to rect.topRight,
+        rect.centerRight to rect.bottomRight,
+    ).forEach { (start, end) ->
+        drawDashedLine(
+            color = selectionColor,
+            start = start,
+            end = end,
+            strokeWidth = strokeWidth,
+            intervals = intervals,
+            phase = phase,
+        )
+    }
 }
+
+@Suppress("LongParameterList")
+internal expect fun DrawScope.drawDashedLine(
+    color: Color,
+    start: Offset,
+    end: Offset,
+    intervals: FloatArray,
+    phase: Float = 0f,
+    strokeWidth: Float = Stroke.HairlineWidth,
+    cap: StrokeCap = Stroke.DefaultCap,
+    @FloatRange(from = 0.0, to = 1.0) alpha: Float = 1.0f,
+    colorFilter: ColorFilter? = null,
+    blendMode: BlendMode = DefaultBlendMode,
+)
 
 data class GridDraggableAnchors2d(
     private val scaledCellPixelSize: Float,
