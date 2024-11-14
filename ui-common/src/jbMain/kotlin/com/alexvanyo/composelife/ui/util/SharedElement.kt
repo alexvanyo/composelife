3a1dd5cfb0497517fa8ba624605cbcde7e04f9bf
Alex Vanyo <vanyo@google.com>
2024-11-13 20:00:58 -0800

    Update shared element API usage with divergence

---
+++ ui-common/src/jbMain/kotlin/com/alexvanyo/composelife/ui/util/SharedElement.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alexvanyo.composelife.ui.util
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.BoundsTransform
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.SharedTransitionScope.OverlayClip
+import androidx.compose.animation.SharedTransitionScope.PlaceHolderSize
+import androidx.compose.animation.SharedTransitionScope.PlaceHolderSize.Companion.contentSize
+import androidx.compose.animation.SharedTransitionScope.SharedContentState
+import androidx.compose.animation.core.Spring.StiffnessMediumLow
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+
+context(SharedTransitionScope)
+@Suppress("LongParameterList")
+@OptIn(ExperimentalSharedTransitionApi::class)
+expect fun Modifier.sharedElement(
+    s: SharedContentState,
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    boundsTransform: BoundsTransform = DefaultBoundsTransform,
+    placeHolderSize: PlaceHolderSize = contentSize,
+    renderInOverlayDuringTransition: Boolean = true,
+    zIndexInOverlay: Float = 0f,
+    clipInOverlayDuringTransition: OverlayClip = ParentClip,
+): Modifier
+
+// Defaults copied from SharedTransitionScope.kt
+
+@ExperimentalSharedTransitionApi
+internal val DefaultBoundsTransform = BoundsTransform { _, _ ->
+    spring(
+        stiffness = StiffnessMediumLow,
+        visibilityThreshold = Rect.VisibilityThreshold,
+    )
+}
+
+@ExperimentalSharedTransitionApi
+internal val ParentClip: OverlayClip =
+    object : OverlayClip {
+        override fun getClipPath(
+            state: SharedContentState,
+            bounds: Rect,
+            layoutDirection: LayoutDirection,
+            density: Density,
+        ): Path? {
+            return state.parentSharedContentState?.clipPathInOverlay
+        }
+    }
