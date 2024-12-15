7861c69d1dc2fd4889611af2e572a7784cc0fbbf
Alex Vanyo <vanyo@google.com>
2024-12-15 13:56:38 -0800

    Re-enable code coverage for device tests
    Fixes #2257

--- ui-app-screenshot-tests/build.gradle.kts
+++ ui-app-screenshot-tests/build.gradle.kts
@@ -46,7 +46,6 @@ kotlin {
         compilations.withType(KotlinMultiplatformAndroidDeviceTestCompilation::class.java) {
             instrumentationRunner = "com.alexvanyo.composelife.test.InjectTestRunner"
         }
-        configureGradleManagedDevices(setOf(FormFactor.Mobile), this)
     }
     jvm("desktop")
 
