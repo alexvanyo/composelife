32b7912aba22538a3da40db99e87ac5f8df6c7a1
Alex Vanyo <vanyo@google.com>
2025-01-02 10:16:21 -0800

    Remove Guava

--- algorithm/build.gradle.kts
+++ algorithm/build.gradle.kts
@@ -82,7 +82,6 @@ kotlin {
         val jvmMain by creating {
             dependsOn(commonMain)
             dependencies {
-                implementation(libs.guava.android)
                 implementation(libs.jetbrains.compose.uiUnit)
                 implementation(libs.sealedEnum.runtime)
             }
