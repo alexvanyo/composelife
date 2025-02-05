301688389e8d7b4fa85936741a3b121a6acc93ab
Alex Vanyo <vanyo@google.com>
2025-02-05 10:40:56 -0800

    Add kotlinx-coroutines-swing dependency where needed

--- ui-cells/build.gradle.kts
+++ ui-cells/build.gradle.kts
@@ -142,6 +142,9 @@ kotlin {
         val desktopTest by getting {
             dependsOn(jbTest)
             configurations["kspDesktopTest"].dependencies.add(libs.kotlinInject.ksp.get())
+            dependencies {
+                implementation(libs.kotlinx.coroutines.swing)
+            }
         }
         val androidSharedTest by getting {
             dependsOn(jbTest)
