47aa42cd2e578aca4d6010f8308e0be08640bcff
Alex Vanyo <vanyo@google.com>
2025-03-08 03:00:19 +0100

    Pattern collections WIP

--- dispatchers/src/commonMain/kotlin/com/alexvanyo/composelife/dispatchers/ComposeLifeDispatchers.kt
+++ dispatchers/src/commonMain/kotlin/com/alexvanyo/composelife/dispatchers/ComposeLifeDispatchers.kt
@@ -16,6 +16,7 @@
 
 package com.alexvanyo.composelife.dispatchers
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
@@ -49,6 +50,8 @@ interface ComposeLifeDispatchers {
      */
     val IO: CoroutineContext
 
+    fun IOWithLimitedParallelism(parallelism: Int): CoroutineDispatcher
+
     /**
      * The [CoroutineContext] for driving cell state ticks, in response to time delays.
      */
@@ -66,5 +69,7 @@ class DefaultComposeLifeDispatchers : ComposeLifeDispatchers {
     override val Main: CoroutineContext = Dispatchers.Main
     override val Unconfined: CoroutineContext = Dispatchers.Unconfined
     override val IO: CoroutineContext = Dispatchers.IO
+    override fun IOWithLimitedParallelism(parallelism: Int): CoroutineDispatcher =
+        Dispatchers.IO.limitedParallelism(parallelism)
     override val CellTicker: CoroutineContext = EmptyCoroutineContext
 }
