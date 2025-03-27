edfeea5da120920efcbe7fc582e6d6191d25d58b
Alex Vanyo <vanyo@google.com>
2025-03-27 14:10:37 -0700

    Add EntryPoint and processor

---
+++ entry-point-runtime/build.gradle.kts
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    alias(libs.plugins.convention.kotlinMultiplatform)
+    alias(libs.plugins.convention.androidLibrary)
+    alias(libs.plugins.convention.androidLibraryKsp)
+    alias(libs.plugins.convention.detekt)
+    alias(libs.plugins.gradleDependenciesSorter)
+}
+
+android {
+    namespace = "com.alexvanyo.composelife.entrypointruntime"
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+kotlin {
+    androidTarget()
+    jvm("desktop")
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinInjectAnvil.runtime)
+            }
+        }
+        val desktopMain by getting {
+            configurations["kspDesktop"].dependencies.addAll(
+                listOf(
+                    libs.kotlinInject.ksp.get(),
+                    libs.kotlinInjectAnvil.ksp.get(),
+                )
+            )
+        }
+        val androidMain by getting {
+            configurations["kspAndroid"].dependencies.addAll(
+                listOf(
+                    libs.kotlinInject.ksp.get(),
+                    libs.kotlinInjectAnvil.ksp.get(),
+                )
+            )
+        }
+    }
+}
