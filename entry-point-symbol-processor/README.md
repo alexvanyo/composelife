9c7863a01dc69f728a12b7d465a59318b5a6233b
Alex Vanyo <vanyo@google.com>
2025-03-27 14:10:37 -0700

    Add EntryPoint and processor

---
+++ entry-point-symbol-processor/README.md
@@ -0,0 +1,4 @@
+# entry-point-symbol-processor
+
+A module defining a symbol processor for KSP that generates concrete implementations of entry
+point interfaces annotated with `@EntryPoint` from [entry-point-runtime](../entry-point-runtime)
---
+++ entry-point-symbol-processor/src/jvmMain/kotlin/com/alexvanyo/composelife/entrypoint/EntryPointSymbolProcessor.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alexvanyo.composelife.entrypoint
+
+import com.google.devtools.ksp.getVisibility
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.symbol.ClassKind
+import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.KSType
+import com.google.devtools.ksp.symbol.Visibility
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asTypeName
+import com.squareup.kotlinpoet.ksp.addOriginatingKSFile
+import com.squareup.kotlinpoet.ksp.toAnnotationSpec
+import com.squareup.kotlinpoet.ksp.toClassName
+import com.squareup.kotlinpoet.ksp.toTypeName
+import com.squareup.kotlinpoet.ksp.toTypeParameterResolver
+import com.squareup.kotlinpoet.ksp.writeTo
+import me.tatarka.inject.annotations.Inject
+import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
+
+class EntryPointSymbolProcessor(
+    private val codeGenerator: CodeGenerator,
+    private val logger: KSPLogger,
+) : SymbolProcessor {
+    override fun process(resolver: Resolver): List<KSAnnotated> = with(logger) {
+        resolver
+            .getSymbolsWithAnnotation(requireNotNull(EntryPoint::class.qualifiedName))
+            .filterIsInstance<KSClassDeclaration>()
+            .onEach {
+                checkIsInterface(it) { "Contributed entry points must be interfaces." }
+                checkIsPublic(it) { "Contributed entry points must be public." }
+            }
+            .forEach {
+                generateEntryPointImplementation(it)
+            }
+
+        emptyList()
+    }
+
+    private fun generateEntryPointImplementation(entryPoint: KSClassDeclaration) {
+        val entryPointImplClassName =
+            entryPoint.toClassName().peerClass(entryPoint.simpleName.asString() + "Impl")
+        val scope = entryPoint.annotations.first { it.annotationType.toTypeName() == EntryPoint::class.asTypeName() }
+            .arguments
+            .first()
+            .value as KSType?
+
+        val fileSpec = FileSpec.builder(entryPointImplClassName)
+            .addType(
+                TypeSpec
+                    .classBuilder(entryPointImplClassName)
+                    .addOriginatingKSFile(requireNotNull(entryPoint.containingFile))
+                    .addAnnotation(Inject::class)
+                    .addAnnotation(
+                        AnnotationSpec.builder(ContributesBinding::class)
+                            .addMember("scope = %T::class", requireNotNull(scope).toClassName())
+                            .build(),
+                    )
+                    .addSuperinterface(entryPoint.toClassName())
+                    .primaryConstructor(
+                        FunSpec.constructorBuilder()
+                            .addParameters(
+                                entryPoint.getAllProperties()
+                                    .map { propertyDeclaration ->
+                                        ParameterSpec.builder(
+                                            name = propertyDeclaration.simpleName.asString(),
+                                            type = propertyDeclaration.type.toTypeName(
+                                                entryPoint.typeParameters.toTypeParameterResolver()
+                                            )
+                                                .copy(
+                                                    annotations = propertyDeclaration.type.annotations
+                                                        .map { it.toAnnotationSpec() }.toList()
+                                                )
+
+                                        )
+                                            .addAnnotations(
+                                                propertyDeclaration.annotations
+                                                    .map { it.toAnnotationSpec() }.toList()
+                                            )
+                                            .build()
+                                    }
+                                    .toList()
+                            )
+
+                            .build(),
+                    )
+                    .addProperties(
+                        entryPoint.getAllProperties()
+                            .map { propertyDeclaration ->
+                                PropertySpec.builder(
+                                    name = propertyDeclaration.simpleName.asString(),
+                                    type = propertyDeclaration.type.toTypeName(
+                                        entryPoint.typeParameters.toTypeParameterResolver()
+                                    )
+                                        .copy(
+                                            annotations = propertyDeclaration.type.annotations
+                                                .map { it.toAnnotationSpec() }.toList()
+                                        )
+                                )
+                                    .addAnnotations(
+                                        propertyDeclaration.annotations
+                                            .map { it.toAnnotationSpec() }.toList()
+                                    )
+                                    .addModifiers(KModifier.OVERRIDE)
+                                    .initializer(propertyDeclaration.simpleName.asString())
+                                    .build()
+                            }
+                            .toList()
+                    )
+                    .build(),
+            )
+            .build()
+
+        fileSpec.writeTo(codeGenerator, aggregating = false)
+    }
+}
+
+context(_: KSPLogger)
+private fun checkIsInterface(
+    clazz: KSClassDeclaration,
+    lazyMessage: () -> String,
+) {
+    check(clazz.classKind == ClassKind.INTERFACE, clazz, lazyMessage)
+}
+
+context(_: KSPLogger)
+private fun checkIsPublic(
+    declaration: KSDeclaration,
+    lazyMessage: () -> String,
+) {
+    check(declaration.getVisibility() == Visibility.PUBLIC, declaration, lazyMessage)
+}
+
+context(logger: KSPLogger)
+private fun check(
+    condition: Boolean,
+    symbol: KSNode?,
+    lazyMessage: () -> String,
+) {
+    if (!condition) {
+        val message = lazyMessage()
+        logger.error(message, symbol)
+        throw IllegalStateException(message)
+    }
+}
