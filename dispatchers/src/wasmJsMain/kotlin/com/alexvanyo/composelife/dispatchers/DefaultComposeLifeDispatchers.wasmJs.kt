781aff635f106c3adf4b504f6b69f311aa03cfb7
Alex Vanyo <vanyo@google.com>
2025-04-16 18:20:14 -0700

    Add wasmJs to easy-to-add modules

---
+++ dispatchers/src/wasmJsMain/kotlin/com/alexvanyo/composelife/dispatchers/DefaultComposeLifeDispatchers.wasmJs.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alexvanyo.composelife.dispatchers
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import me.tatarka.inject.annotations.Inject
+import software.amazon.lastmile.kotlin.inject.anvil.AppScope
+import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
+import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+/**
+ * The default implementation of [ComposeLifeDispatchers], which just delegates to the normal [Dispatchers] versions.
+ */
+@Inject
+@ContributesBinding(AppScope::class)
+@SingleIn(AppScope::class)
+class DefaultComposeLifeDispatchers : ComposeLifeDispatchers {
+    override val Default: CoroutineContext = Dispatchers.Default
+    override val Main: CoroutineContext = Dispatchers.Main
+    override val Unconfined: CoroutineContext = Dispatchers.Unconfined
+    override val IO: CoroutineContext = Dispatchers.Default
+    override fun IOWithLimitedParallelism(parallelism: Int): CoroutineDispatcher =
+        Dispatchers.Default.limitedParallelism(parallelism)
+    override val CellTicker: CoroutineContext = EmptyCoroutineContext
+}
