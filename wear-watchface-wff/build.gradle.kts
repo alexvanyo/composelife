8e0c65b3f4c8d8035f0c51dc0f25a4995dc265c4
Alex Vanyo <vanyo@google.com>
2025-05-11 23:03:25 -0700

    WFF WIP

---
+++ wear-watchface-wff/build.gradle.kts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.alexvanyo.composelife.buildlogic.FormFactor
+import com.alexvanyo.composelife.buildlogic.configureGradleManagedDevices
+
+plugins {
+    alias(libs.plugins.convention.kotlinMultiplatform)
+    alias(libs.plugins.convention.androidApplication)
+    alias(libs.plugins.convention.androidApplicationTesting)
+    alias(libs.plugins.convention.detekt)
+    alias(libs.plugins.gradleDependenciesSorter)
+}
+
+android {
+    namespace = "com.alexvanyo.composelife"
+    defaultConfig {
+        applicationId = "com.alexvanyo.composelife"
+        minSdk = 33
+        targetSdk = 35
+    }
+    configureGradleManagedDevices(setOf(FormFactor.Wear), this)
+}
+
+kotlin {
+    androidTarget()
+
+    sourceSets {
+        val commonTest by getting {
+            dependencies {
+                implementation(projects.algorithm)
+                implementation(projects.dispatchersTest)
+                implementation(projects.geometry)
+            }
+        }
+        val androidUnitTest by getting {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.test)
+                implementation(libs.testParameterInjector.junit4)
+                implementation(libs.turbine)
+            }
+        }
+    }
+}
