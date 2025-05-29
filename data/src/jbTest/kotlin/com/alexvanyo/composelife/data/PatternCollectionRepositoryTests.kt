2fb52e1a79102c55ab0362a2d84aeecb528e3159
Alex Vanyo <vanyo@google.com>
2025-05-28 21:11:35 -0700

    Write cell states to files
    Fixes #2427 and fixes #2463
    
    Adds a column into the cell state database to allow saving patterns
    as files, referenced by a file name.
    
    This also adds support for synchronized pattern collections to
    populate the cell state repository, with the list of extract patterns.
    
    Autosaved cell states are saved into a specific autosaved cell state
    folder, which is kept separate from the synchronized pattern collection
    folders. This allows more easily creating, updating, and removing
    both pattern collections and the associated cell states.
    
    Future work includes:
    - Allowing a working space for explicitly saved and imported patterns
    - Allow browsing/searching/deleting patterns
    - Allow configuring pruning frequency

--- data/src/jbTest/kotlin/com/alexvanyo/composelife/data/PatternCollectionRepositoryTests.kt
+++ data/src/jbTest/kotlin/com/alexvanyo/composelife/data/PatternCollectionRepositoryTests.kt
@@ -16,33 +16,47 @@
 
 package com.alexvanyo.composelife.data
 
+import com.alexvanyo.composelife.data.model.CellStateMetadata
 import com.alexvanyo.composelife.data.model.PatternCollection
+import com.alexvanyo.composelife.database.CellState
+import com.alexvanyo.composelife.database.CellStateQueries
 import com.alexvanyo.composelife.database.PatternCollectionQueries
+import com.alexvanyo.composelife.dispatchers.GeneralTestDispatcher
 import com.alexvanyo.composelife.entrypoint.EntryPoint
 import com.alexvanyo.composelife.entrypoint.EntryPointProvider
-import com.alexvanyo.composelife.dispatchers.GeneralTestDispatcher
+import com.alexvanyo.composelife.filesystem.PersistedDataPath
+import com.alexvanyo.composelife.model.MacrocellCellStateSerializer
+import com.alexvanyo.composelife.model.toCellState
 import com.alexvanyo.composelife.network.FakeRequestHandler
 import com.alexvanyo.composelife.resourcestate.ResourceState
 import com.alexvanyo.composelife.test.BaseInjectTest
 import io.ktor.client.engine.mock.respond
-import kotlinx.coroutines.async
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.datetime.Instant
+import okio.Path
+import okio.Path.Companion.toPath
+import okio.fakefilesystem.FakeFileSystem
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import kotlin.reflect.KClass
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 @EntryPoint(AppScope::class)
 interface PatternCollectionRepositoryTestsEntryPoint {
     val patternCollectionRepository: PatternCollectionRepository
     val patternCollectionQueries: PatternCollectionQueries
+    val cellStateRepository: CellStateRepository
+    val cellStateQueries: CellStateQueries
     val generalTestDispatcher: @GeneralTestDispatcher TestDispatcher
     val fakeRequestHandler: FakeRequestHandler
+    val fakeFileSystem: FakeFileSystem
+    val persistedDataPath: @PersistedDataPath Path
 }
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -54,11 +68,19 @@ class PatternCollectionRepositoryTests : BaseInjectTest<TestComposeLifeApplicati
     private val patternCollectionRepository: PatternCollectionRepository
         get() = entryPoint.patternCollectionRepository
 
+    private val cellStateRepository get() = entryPoint.cellStateRepository
+
     private val patternCollectionQueries get() = entryPoint.patternCollectionQueries
+
+    private val cellStateQueries get() = entryPoint.cellStateQueries
 
     private val testDispatcher get() = entryPoint.generalTestDispatcher
 
     private val fakeRequestHandler: FakeRequestHandler get() = entryPoint.fakeRequestHandler
+
+    private val fakeFileSystem get() = entryPoint.fakeFileSystem
+
+    private val persistedDataPath get() = entryPoint.persistedDataPath
 
     @Test
     fun pattern_collection_is_empty_initially() = runAppTest(testDispatcher) {
@@ -113,7 +135,6 @@ class PatternCollectionRepositoryTests : BaseInjectTest<TestComposeLifeApplicati
             sourceUrl = "https://alex.vanyo.dev/composelife/patterns.zip",
         )
 
-
         val synchronizationJob = async {
             patternCollectionRepository.synchronizePatternCollections()
         }
@@ -161,6 +182,124 @@ class PatternCollectionRepositoryTests : BaseInjectTest<TestComposeLifeApplicati
                 )
             ),
             patternCollectionRepository.collections,
+        )
+
+        // Validate that the cell state was extracted on disk
+        val cellStateEntity = cellStateQueries
+            .getCellStatesByPatternCollectionId(patternCollectionId)
+            .executeAsOne()
+
+        assertNotNull(cellStateEntity)
+        val serializedCellStateFile = cellStateEntity.serializedCellStateFile
+        assertNotNull(serializedCellStateFile)
+        assertEquals(
+            "PatternCollections/${patternCollectionId.value}/extracted/pulsarpixeldisplay.mc",
+            serializedCellStateFile,
+        )
+        val expectedPath = serializedCellStateFile.toPath()
+        assertEquals(
+            CellState(
+                id = cellStateEntity.id,
+                name = null,
+                description = null,
+                generation = 0,
+                formatExtension = "mc",
+                serializedCellState = null,
+                serializedCellStateFile = expectedPath.toString(),
+                wasAutosaved = false,
+                patternCollectionId = patternCollectionId,
+            ),
+            cellStateEntity,
+        )
+        assertEquals(
+            setOf(
+                "PatternCollections".toPath(),
+                "PatternCollections/${patternCollectionId.value}".toPath(),
+                "PatternCollections/${patternCollectionId.value}/archive.zip".toPath(),
+                "PatternCollections/${patternCollectionId.value}/archive.sha256".toPath(),
+                "PatternCollections/${patternCollectionId.value}/extracted".toPath(),
+                "PatternCollections/${patternCollectionId.value}/extracted/pulsarpixeldisplay.mc".toPath(),
+            ),
+            fakeFileSystem.listRecursively(persistedDataPath)
+                .map { it.relativeTo(persistedDataPath) }
+                .toSet(),
+        )
+        assertEquals(
+            this::class.java
+                .getResource("/patternfiles/pulsarpixeldisplay.mc")!!
+                .readText(),
+            fakeFileSystem.read(persistedDataPath / expectedPath) { readUtf8() },
+        )
+
+        // Validate that the cell state is queryable through the cell state repository
+        val cellStates = cellStateRepository.getCellStates()
+        assertEquals(1, cellStates.size)
+        val cellStateId = cellStates.first().cellStateMetadata.id
+        assertEquals(
+            CellStateMetadata(
+                id = cellStateId,
+                name = null,
+                description = null,
+                generation = 0,
+                wasAutosaved = false,
+                patternCollectionId = patternCollectionId,
+            ),
+            cellStates.first().cellStateMetadata,
+        )
+        assertEquals(
+            this::class.java
+                .getResource("/patternfiles/pulsarpixeldisplay.mc")!!
+                .readText()
+                .toCellState(fixedFormatCellStateSerializer = MacrocellCellStateSerializer),
+            cellStates.first().cellState
+        )
+    }
+
+    @Test
+    fun deleting_pattern_collection_removes_files() = runAppTest(testDispatcher) {
+        backgroundScope.launch {
+            patternCollectionRepository.observePatternCollections()
+        }
+
+        val patternCollectionId = patternCollectionRepository.addPatternCollection(
+            sourceUrl = "https://alex.vanyo.dev/composelife/patterns.zip",
+        )
+
+        val synchronizationJob = async {
+            patternCollectionRepository.synchronizePatternCollections()
+        }
+
+        runCurrent()
+
+        fakeRequestHandler.addRequestHandler { request ->
+            assertEquals("https://alex.vanyo.dev/composelife/patterns.zip", request.url.toString())
+            respond(
+                this::class.java
+                    .getResource("/patternfiles/patterns.zip")!!
+                    .readBytes()
+            )
+        }
+        assertTrue(synchronizationJob.await())
+
+        patternCollectionRepository.deletePatternCollection(patternCollectionId)
+
+        runCurrent()
+
+        assertEquals(
+            ResourceState.Success(emptyList(),),
+            patternCollectionRepository.collections,
+        )
+        assertEquals(
+            emptyList(),
+            cellStateRepository.getCellStates(),
+        )
+        assertEquals(
+            setOf(
+                "PatternCollections".toPath(),
+            ),
+            fakeFileSystem.listRecursively(persistedDataPath)
+                .map { it.relativeTo(persistedDataPath) }
+                .toSet(),
         )
     }
 }
--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -139,6 +139,7 @@ sqldelight {
             packageName.set("com.alexvanyo.composelife.database")
             schemaOutputDirectory.set(file("src/commonMain/sqldelight/databases"))
             verifyMigrations.set(true)
+            verifyDefinitions.set(true)
         }
     }
 }
