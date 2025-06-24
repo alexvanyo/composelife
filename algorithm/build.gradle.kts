9a54b864a74cfd98581fe22c3875c81e7a05bb32
Alex Vanyo <vanyo@google.com>
2025-06-24 09:41:08 -0700

    Add config wasmJs support to algorithm and patterns

--- algorithm/build.gradle.kts
+++ algorithm/build.gradle.kts
@@ -16,6 +16,7 @@
 
 import com.alexvanyo.composelife.buildlogic.FormFactor
 import com.alexvanyo.composelife.buildlogic.configureGradleManagedDevices
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.convention.kotlinMultiplatform)
@@ -49,6 +50,16 @@ kotlin {
 
         testRuns {
             create("molecule") { setExecutionSourceFrom(moleculeTest) }
+        }
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            testTask {
+                useKarma {
+                    useChromiumHeadless()
+                }
+            }
         }
     }
 
