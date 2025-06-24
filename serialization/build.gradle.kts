3dc254d80270d3c7c385e80828447beb1d33704b
Alex Vanyo <vanyo@google.com>
2025-06-24 17:31:22 -0400

    Add full wasmJs support to serialization with JetBrains SavedState

--- serialization/build.gradle.kts
+++ serialization/build.gradle.kts
@@ -57,28 +57,31 @@ kotlin {
             dependencies {
                 api(libs.jetbrains.compose.runtime)
                 api(libs.jetbrains.compose.runtime.saveable)
+                api(libs.jetbrains.savedState)
+                api(libs.jetbrains.savedState.compose)
                 api(libs.kotlinx.serialization.core)
+                api(projects.sealedEnum.runtime)
             }
         }
-        val jvmMain by creating {
+        val jbMain by creating {
             dependsOn(commonMain)
             dependencies {
-                api(libs.androidx.savedState)
-                api(libs.androidx.savedState.compose)
                 api(libs.jetbrains.compose.uiGeometry)
                 api(libs.jetbrains.compose.uiUnit)
-                api(projects.sealedEnum.runtime)
 
                 implementation(libs.jetbrains.compose.uiUtil)
             }
         }
-        val jbMain by creating {
-            dependsOn(jvmMain)
-        }
-        val desktopMain by getting {
+        val jvmMain by creating {
             dependsOn(jbMain)
         }
+        val desktopMain by getting {
+            dependsOn(jvmMain)
+        }
         val androidMain by getting {
+            dependsOn(jvmMain)
+        }
+        val wasmJsMain by getting {
             dependsOn(jbMain)
         }
         val commonTest by getting {
@@ -90,23 +93,26 @@ kotlin {
                 implementation(projects.testActivity)
             }
         }
-        val jvmTest by creating {
-            dependsOn(commonTest)
-        }
         val jbTest by creating {
-            dependsOn(jvmTest)
+            dependsOn(commonTest)
             dependencies {
                 implementation(libs.jetbrains.compose.foundation)
                 implementation(libs.jetbrains.compose.uiTest)
             }
         }
-        val desktopTest by getting {
+        val jvmTest by creating {
             dependsOn(jbTest)
+        }
+        val desktopTest by getting {
+            dependsOn(jvmTest)
             dependencies {
                 implementation(compose.desktop.currentOs)
             }
         }
         val androidSharedTest by getting {
+            dependsOn(jvmTest)
+        }
+        val wasmJsTest by getting {
             dependsOn(jbTest)
         }
     }
