e49a93aa35fcaa0e8d975847debc5816c6e2d87b
Alex Vanyo <vanyo@google.com>
2025-08-15 18:04:11 -0700

    Replace circuit-retained with Compose retain
    Fixes #2789

--- navigation/src/jbMain/kotlin/com/alexvanyo/composelife/navigation/AssociateWithRenderablePanes.kt
+++ navigation/src/jbMain/kotlin/com/alexvanyo/composelife/navigation/AssociateWithRenderablePanes.kt
@@ -17,13 +17,23 @@
 package com.alexvanyo.composelife.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ControlledRetainScope
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.ForgetfulRetainScope
+import androidx.compose.runtime.LocalRetainScope
+import androidx.compose.runtime.RetainStateProvider
+import androidx.compose.runtime.currentComposer
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.retain
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
-import com.slack.circuit.retained.rememberRetainedStateHolder
+import androidx.compose.runtime.setValue
+import kotlin.uuid.Uuid
 
 /**
  * Returns a [RenderableNavigationState] associating the entries for the [navigationState] with a lambda to
@@ -32,34 +42,34 @@ import com.slack.circuit.retained.rememberRetainedStateHolder
  * This transforms [pane], a general lambda to render a [NavigationEntry] of type [T], into instance specific
  * lambdas for each navigation entry.
  */
+@Suppress("LongMethod")
 @Composable
 fun <T : NavigationEntry, S : NavigationState<T>> associateWithRenderablePanes(
     navigationState: S,
     pane: @Composable (T) -> Unit,
 ): RenderableNavigationState<T, S> {
     val saveableStateHolder = rememberSaveableStateHolder()
-    val retainedStateHolder = rememberRetainedStateHolder()
+    val fanOutRetainScopeProvider = retainFanOutRetainScopeProvider<Uuid>()
 
     val currentEntryKeySet by rememberUpdatedState(navigationState.entryMap.keys.toSet())
 
     // Set up a DisposableEffect for each entry key in the backstack to clear a particular entry's state from the
-    // retainedStateRegistry when the entry is no longer in the backstack.
+    // saveableStateHolder when the entry is no longer in the backstack.
     // Without this, entry state would leak and be kept around indefinitely in the case where an entry is removed
     // from the backstack while not currently being rendered.
     currentEntryKeySet.forEach { entryKey ->
         key(entryKey) {
             DisposableEffect(Unit) {
                 // Ordering notes
-                // This will happen _after_ the RetainedStateProvider's onDispose in
-                // due to LIFO ordering of DisposableEffect, so any retained entry state that was saved will be cleared.
-                // If the entry is still visible, then the can retain checker below will avoiding retaining the state
-                // if saved by a parent.
                 // This will happen _after_ the content's onDispose in SaveableStateProvider due to
                 // LIFO ordering of DisposableEffect, so any saved state will be cleared. If the entry is still visible,
                 // then removing this state will prevent the entry state from being saved when it disappears.
+                // This will happen _after_ the content's retain onDispose s due to
+                // LIFO ordering of DisposableEffect, so any retained values will be cleared. If the entry is still
+                // visible, then it won't start saving in the DisposableEffect below
                 onDispose {
                     if (entryKey !in currentEntryKeySet) {
-                        retainedStateHolder.removeState(entryKey.toString())
+                        fanOutRetainScopeProvider.clearChild(entryKey)
                         saveableStateHolder.removeState(entryKey)
                     }
                 }
@@ -67,10 +77,35 @@ fun <T : NavigationEntry, S : NavigationState<T>> associateWithRenderablePanes(
         }
     }
 
+    DisposableEffect(Unit) {
+        fanOutRetainScopeProvider.clearChildren { !currentEntryKeySet.contains(it) }
+        onDispose {}
+    }
+
     val wrappedPane: @Composable (T) -> Unit = { entry ->
         saveableStateHolder.SaveableStateProvider(key = entry.id) {
-            retainedStateHolder.RetainedStateProvider(key = entry.id.toString()) {
+            val entryRetainScope = remember {
+                fanOutRetainScopeProvider.getOrCreateRetainScopeForChild(entry.id)
+            }
+            CompositionLocalProvider(LocalRetainScope provides entryRetainScope) {
                 pane(entry)
+            }
+            val composer = currentComposer
+
+            @Suppress("ComposeRememberMissing")
+            var isKeepingExitedValues by retain { mutableStateOf(false) }
+            DisposableEffect(entry.id) {
+                val endRetainHandle = composer.scheduleFrameEndCallback {
+                    if (isKeepingExitedValues) {
+                        fanOutRetainScopeProvider.endRetainChild(entry.id)
+                        isKeepingExitedValues = false
+                    }
+                }
+                onDispose {
+                    endRetainHandle.cancel()
+                    isKeepingExitedValues = true
+                    fanOutRetainScopeProvider.beginRetainChild(entry.id)
+                }
             }
         }
     }
