f5e3305affffc2617ade26eab9c602eb40b13ab1
Alex Vanyo <alex@vanyo.dev>
2025-09-28 11:15:58 -0700

    Remove snapshot dependency for retain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,6 @@ dependencyResolutionManagement {
         }
         google()
         mavenCentral()
-        maven(url = "https://androidx.dev/snapshots/builds/14137143/artifacts/repository")
     }
 }
 
