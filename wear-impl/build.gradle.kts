8d02e5e5df8e8e0aa589aebdb1131ef046211811
Alex Vanyo <alex@vanyo.dev>
2025-10-11 19:41:04 -0700

    Shell out app and wear modules into android-application, app-impl, app-test-fixtures, and wear-impl
    Fixes #2955

---
+++ wear-impl/build.gradle.kts
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.alexvanyo.composelife.buildlogic.FormFactor
+import com.alexvanyo.composelife.buildlogic.configureGradleManagedDevices
+
+plugins {
+    alias(libs.plugins.convention.kotlinMultiplatform)
+    alias(libs.plugins.convention.androidLibrary)
+    alias(libs.plugins.convention.androidLibraryCompose)
+    alias(libs.plugins.convention.detekt)
+    alias(libs.plugins.androidx.baselineProfile)
+    alias(libs.plugins.metro)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.alexvanyo.composelife.wearimpl"
+        minSdk = 26
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(projects.androidApplication)
+                implementation(projects.filesystem)
+                implementation(projects.injectScopes)
+                implementation(projects.processLifecycle)
+                implementation(projects.strictMode)
+                implementation(projects.wearWatchface)
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.androidx.lifecycle.runtime)
+            }
+        }
+    }
+}
