54a87e00bbf257f53e9f589a3f1135243b9424be
alexvanyo <alexvanyo@users.noreply.github.com>
2025-11-30 18:22:31 +0000

    deploy: 375e1c40420f5377cadccdf3849f29e8df3446b8

--- web-app.js
+++ web-app.js
@@ -81,7 +81,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
-module.exports = __webpack_require__.p + "5b91cafc45564e97815d.wasm";
+module.exports = __webpack_require__.p + "61bd7b605e2d25543736.wasm";
 
 /***/ }),
 
